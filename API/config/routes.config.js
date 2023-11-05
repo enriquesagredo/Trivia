a78d314310155be875dae192e9ea890be6294b30
enriquesagredo <enrique.sagredo@protonmail.com>
2023-11-05 02:39:37 +0100

    feat. first routes , list

--- API/config/routes.config.js
+++ API/config/routes.config.js
@@ -1,4 +1,7 @@
 const express = require("express")
 const router = express.Router()
+const users = require("../controller/user.controller")
+
+router.get("/users", users.list)
 
 module.exports = router;
